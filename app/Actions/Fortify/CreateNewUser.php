e035587e87fa9eac8b1cb0c81f891c8e336dcaca
Bana Khusnan <banakhsn04@gmail.com>
2022-09-02 10:39:45 +0700

    Merubah nama field dan menambahkan property baru

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -21,7 +21,7 @@ class CreateNewUser implements CreatesNewUsers
     public function create(array $input)
     {
         Validator::make($input, [
-            'name' => ['required', 'string', 'max:255'],
+            'nama' => ['required', 'string', 'max:255'],
             'email' => [
                 'required',
                 'string',
@@ -29,14 +29,15 @@ class CreateNewUser implements CreatesNewUsers
                 'max:255',
                 Rule::unique(User::class),
             ],
+            'no_hp' => ['required', 'min:10', 'max:14'],
             'password' => $this->passwordRules(),
         ])->validate();
 
         return User::create([
-            'name' => $input['name'],
-            'no_induk' => $input['no_induk'],
+            'nama' => $input['nama'],
             'role' => $input['role'],
             'email' => $input['email'],
+            'no_hp' => $input['no_hp'],
             'password' => Hash::make($input['password']),
         ]);
     }
